701650ffbc802ea70a367712d2ed1d5ed7c1ea3c
GabiMazzol <166437971+GabiMazzol@users.noreply.github.com>
2024-06-10 23:09:36 -0300

    Update Burger.Robot.c

--- TP3/Burger.Robot.c
+++ TP3/Burger.Robot.c
@@ -24,7 +24,7 @@ void *comer_hamburguesa(void *tid)
 		else
 		{
 			printf("SE TERMINARON LAS HAMBURGUESAS :( \n");
-
+			 turno = (turno + 1) % NUMBER_OF_THREADS; 
 			pthread_exit(NULL); // forzar terminacion del hilo
 		}
     // SALIDA DE LA ZONA CRÍTICA  
